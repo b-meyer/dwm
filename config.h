82cc3f483a79d5a25c6fa14a1ff5318d51464ea5
Blake Meyer <blakewmeyer@yahoo.com>
2023-08-02 07:46:23 -0500

    bottom bar

--- config.h
+++ config.h
@@ -5,7 +5,7 @@ static unsigned int borderpx  = 2;        /* border pixel of windows */
 static unsigned int gappx     = 8;        /* gaps between windows */
 static unsigned int snap      = 32;       /* snap pixel */
 static int showbar            = 1;        /* 0 means no bar */
-static int topbar             = 1;        /* 0 means bottom bar */
+static int topbar             = 0;        /* 0 means bottom bar */
 static int vertpad            = 8;        /* vertical padding of bar */
 static int sidepad            = 8;        /* horizontal padding of bar */
 static char font[]            = "monospace:size=10";
